464f77d11a62c61712fc69e29798c5d939791e7c
Kyle Rehak <kylerehak@gmail.com>
2023-01-15 13:43:41 +0800

    Refactor into functional component

--- src/components/ExperienceInputs.js
+++ src/components/ExperienceInputs.js
@@ -1,72 +1,64 @@
 import React from 'react';
 import TextInput from './TextInput';
 
-class ExperienceInputs extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
+const ExperienceInputs = (props) => {
+    
+    const handleChange = (e) => {
+        props.updateInfo(e.target, 'exp', e.target.dataset.key);
     }
-
-    handleChange(e) {
-        this.props.updateInfo(e.target, 'exp', e.target.dataset.key);
-    }
-
-    render() {
-        const exp = this.props.exp;
-        return (
-            <div>
-                <h2>Experience</h2>
-                {exp.map((item, index) => {
-                    console.log(item);
-                    console.log(index);
-                    return ( 
-                        <div>
-                            <TextInput 
-                                name='position' 
-                                placeholder='Position' 
-                                onChange={this.handleChange} 
-                                value={item.position}
-                                datakey={index}
-                            />
-                            <TextInput 
-                                name='company' 
-                                placeholder='Company'
-                                onChange={this.handleChange} 
-                                value={item.company}
-                                datakey={index} 
-                            />
-                            <TextInput 
-                                name='city' 
-                                placeholder='City' 
-                                onChange={this.handleChange} 
-                                value={item.city}
-                                datakey={index}
-                            />
-                            <TextInput 
-                                name='from' 
-                                placeholder='From' 
-                                onChange={this.handleChange} 
-                                value={item.from}
-                                datakey={index}
-                            />
-                            <TextInput 
-                                name='to' 
-                                placeholder='To' 
-                                onChange={this.handleChange} 
-                                value={item.to}
-                                datakey={index}
-                            />
-                            <button onClick={() => this.props.deleteExp(index)} type='button'>
-                                Delete
-                            </button>
-                        </div>
-                    );
-                })
-                }
-                <button onClick={this.props.createNewExp} type='button'>+ Add Experience</button>
-            </div>
-        );
-    }
+    
+        const exp = props.exp;
+    return (
+        <div>
+            <h2>Experience</h2>
+            {exp.map((item, index) => {
+                return ( 
+                    <div>
+                        <TextInput 
+                            name='position' 
+                            placeholder='Position' 
+                            onChange={handleChange} 
+                            value={item.position}
+                            datakey={index}
+                        />
+                        <TextInput 
+                            name='company' 
+                            placeholder='Company'
+                            onChange={handleChange} 
+                            value={item.company}
+                            datakey={index} 
+                        />
+                        <TextInput 
+                            name='city' 
+                            placeholder='City' 
+                            onChange={handleChange} 
+                            value={item.city}
+                            datakey={index}
+                        />
+                        <TextInput 
+                            name='from' 
+                            placeholder='From' 
+                            onChange={handleChange} 
+                            value={item.from}
+                            datakey={index}
+                        />
+                        <TextInput 
+                            name='to' 
+                            placeholder='To' 
+                            onChange={handleChange} 
+                            value={item.to}
+                            datakey={index}
+                        />
+                        <button onClick={() => props.deleteExp(index)} type='button'>
+                            Delete
+                        </button>
+                    </div>
+                );
+            })
+            }
+            <button onClick={props.createNewExp} type='button'>+ Add Experience</button>
+        </div>
+    );
 }
 
 export default ExperienceInputs

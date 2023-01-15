befb164ea35b67bb44f69e788f0fe66eb7d8d1d8
Kyle Rehak <kylerehak@gmail.com>
2023-01-15 13:27:27 +0800

    Refactor into functional component

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,24 +1,18 @@
 import React from 'react';
 
-class TextInput extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        const { name, placeholder, onChange, value, datakey } = this.props;
-        return (
-            <input 
-                type='text' 
-                id={name} 
-                name={name} 
-                placeholder={placeholder} 
-                onChange={onChange} 
-                value={value}
-                data-key={datakey}
-            />
-        );
-    }
+const TextInput = (props) => {
+    const { name, placeholder, onChange, value, datakey } = props;
+    return (
+        <input 
+            type='text' 
+            id={name} 
+            name={name} 
+            placeholder={placeholder} 
+            onChange={onChange} 
+            value={value}
+            data-key={datakey}
+        />
+    );
 }
 
 export default TextInput

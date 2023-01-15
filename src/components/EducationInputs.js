a890c4cee6d2c1069c09faeea299f06cc1cb7853
Kyle Rehak <kylerehak@gmail.com>
2023-01-15 13:47:40 +0800

    Refactor into functional component

--- src/components/EducationInputs.js
+++ src/components/EducationInputs.js
@@ -1,63 +1,56 @@
 import React from 'react';
 import TextInput from './TextInput';
 
-class EducationInputs extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
+const EducationInputs = (props) => {
+    
+    const handleChange = (e) => {
+        props.updateInfo(e.target, 'edu', e.target.dataset.key);
     }
 
-    handleChange(e) {
-        this.props.updateInfo(e.target, 'edu', e.target.dataset.key);
-    }
-
-    render() {
-        const edu = [...this.props.edu];
-        return (
-            <div>
-                <h2>Education</h2>
-                    {edu.map((item, index) => {
-                        return (
-                            <div>
-                                <TextInput 
-                                    name='university' 
-                                    placeholder='University Name'
-                                    onChange={this.handleChange}
-                                    value={item.university}
-                                    datakey={index}
-                                />
-                                <TextInput 
-                                    name='degree' 
-                                    placeholder='Degree'
-                                    onChange={this.handleChange}
-                                    value={item.degree}
-                                    datakey={index}
-                                />
-                                <TextInput 
-                                    name='from' 
-                                    placeholder='From'
-                                    onChange={this.handleChange}
-                                    value={item.from}
-                                    datakey={index} 
-                                />
-                                <TextInput 
-                                    name='to' 
-                                    placeholder='To'
-                                    onChange={this.handleChange}
-                                    value={item.to}
-                                    datakey={index} 
-                                />
-                                <button onClick={() => this.props.deleteEdu(index)} type='button'>
-                                    Delete
-                                </button>
-                            </div>
-                        )
-
-                    })}
-                <button onClick={this.props.createNewEdu} type='button'>+ Add Education</button>
-            </div>
-        );
-    }
+    const edu = [...props.edu];
+    return (
+        <div>
+            <h2>Education</h2>
+                {edu.map((item, index) => {
+                    return (
+                        <div>
+                            <TextInput 
+                                name='university' 
+                                placeholder='University Name'
+                                onChange={handleChange}
+                                value={item.university}
+                                datakey={index}
+                            />
+                            <TextInput 
+                                name='degree' 
+                                placeholder='Degree'
+                                onChange={handleChange}
+                                value={item.degree}
+                                datakey={index}
+                            />
+                            <TextInput 
+                                name='from' 
+                                placeholder='From'
+                                onChange={handleChange}
+                                value={item.from}
+                                datakey={index} 
+                            />
+                            <TextInput 
+                                name='to' 
+                                placeholder='To'
+                                onChange={handleChange}
+                                value={item.to}
+                                datakey={index} 
+                            />
+                            <button onClick={() => props.deleteEdu(index)} type='button'>
+                                Delete
+                            </button>
+                        </div>
+                    )
+                })}
+            <button onClick={props.createNewEdu} type='button'>+ Add Education</button>
+        </div>
+    );
 }
 
 export default EducationInputs
